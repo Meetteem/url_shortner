94c2f9c80e05bc19676751375977486ffc1fa854
Meetteem <meetpanchal000004@gmail.com>
2023-09-10 19:43:07 +0530

    Finall

--- routes/url.js
+++ routes/url.js
@@ -6,7 +6,6 @@ router.post('/',handleGenerateNewShortURL);
 router.get('/analytics/:shortId',handleGetAnalytics);
 router.get('/:shortId',async (req,res)=>{
 	const shortId=req.params.shortId
-	try {
 		const entry = await URL.findOneAndUpdate({shortId},{
 			$push :{
 			 visitHistory:{
@@ -14,9 +13,7 @@ router.get('/:shortId',async (req,res)=>{
 			 }
 			}
 		})
-	} catch (error) {
-		
-	}
+	
 	if(!entry.redirectUrl){
 		console.log("Null");
 	}
